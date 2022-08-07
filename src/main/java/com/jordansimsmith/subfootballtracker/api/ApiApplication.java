4a7c5cbabf2ab94748cb379b88bbadefc116eaca
Jordan Sim-Smith <jordansimsmith@gmail.com>
2022-08-07 13:25:09 +1200

    Configure jobrunr for recurring execution

--- src/main/java/com/jordansimsmith/subfootballtracker/api/ApiApplication.java
+++ src/main/java/com/jordansimsmith/subfootballtracker/api/ApiApplication.java
@@ -1,13 +1,30 @@
 package com.jordansimsmith.subfootballtracker.api;
 
+import com.jordansimsmith.subfootballtracker.api.content.ContentService;
+import org.jobrunr.scheduling.JobScheduler;
+import org.jobrunr.scheduling.cron.Cron;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
+    private final JobScheduler jobScheduler;
 
+    @Autowired
+    public ApiApplication(JobScheduler jobScheduler) {
+        this.jobScheduler = jobScheduler;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+    @PostConstruct
+    public void scheduleRecurrently() {
+        jobScheduler.scheduleRecurrently(Cron.daily(), ContentService::checkForUpdates);
+    }
 }
